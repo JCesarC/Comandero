5a18dd01d21237e9e78debf812ae5b089a82e99f
JCesarC <julio.jccp.1@gmail.com>
2022-03-31 16:52:04 -0600

    removed-api

--- API/src/routes/signup.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import SignupController from '../controller/SignupController';
-
-const router = Router();
-
-//login
-router.post('/signupCliente', SignupController.signup);
-
-export default router;
